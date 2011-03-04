863658d8753c0b94c2f6badb32c37c3d45c3796a
Chris Didyk <cdidyk@gmail.com>
2011-03-04 16:18:55 -0500

    adds ability to filter a time_by_project report by an updated_since date

--- lib/harvest/api/reports.rb
+++ lib/harvest/api/reports.rb
@@ -1,30 +1,33 @@
+require 'cgi'
+
 module Harvest
   module API
     class Reports < Base
-      
-      def time_by_project(project, start_date, end_date, user = nil)
+
+      def time_by_project(project, start_date, end_date, user = nil, filters = {})
         query = {:from => start_date.strftime("%Y%m%d"), :to => end_date.strftime("%Y%m%d")}
         query[:user_id] = user.to_i if user
-        
+        query[:updated_since] = updated_since(filters[:updated_since]) if filters[:updated_since]
+
         response = request(:get, credentials, "/projects/#{project.to_i}/entries", :query => query)
-        Harvest::TimeEntry.parse(massage_xml(response.body))
+#        Harvest::TimeEntry.parse(massage_xml(response.body))
       end
-      
+
       def time_by_user(user, start_date, end_date, project = nil)
         query = {:from => start_date.strftime("%Y%m%d"), :to => end_date.strftime("%Y%m%d")}
         query[:project_id] = project.to_i if project
-        
+
         response = request(:get, credentials, "/people/#{user.to_i}/entries", :query => query)
         Harvest::TimeEntry.parse(massage_xml(response.body))
       end
-      
+
       def expenses_by_user(user, start_date, end_date)
         query = {:from => start_date.strftime("%Y%m%d"), :to => end_date.strftime("%Y%m%d")}
-        
+
         response = request(:get, credentials, "/people/#{user.to_i}/expenses", :query => query)
         Harvest::Expense.parse(response.body)
       end
-      
+
       private
         def massage_xml(original_xml)
           # this needs to be done because of the differences in dashes and underscores in the harvest api
@@ -33,6 +36,11 @@ module Harvest
             xml = xml.gsub(dash_field, dash_field.gsub("-", "_"))
           end
           xml
+        end
+
+        def updated_since(since_date)
+          since_date = DateTime.parse(since_date) if since_date.kind_of? String
+          CGI::escape since_date.new_offset.to_s   # by default, new_offset is UTC
         end
     end
   end
---
+++ spec/harvest/reports_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+require File.join(File.expand_path(File.dirname(__FILE__)), "../../lib/harvest/api/reports")
+
+describe Harvest::API::Reports do
+  describe "#time_by_project" do
+    it "should include the updated_since filter as a URL-encoded UTC date time value if supplied" do
+      Harvest::TimeEntry.stub!(:parse)
+
+      HTTParty.
+        should_receive(:send).
+        with(:get, anything, hash_including(:query => {:from => "20101101", :to => "20101110", :updated_since => "2010-12-01T00%3A00%3A00%2B00%3A00"})).
+        and_return double("Response", :code => 200)
+
+      creds = Harvest::Credentials.new 'subdomain', 'user', 'password'
+      report =
+        Harvest::API::Reports.new(creds).
+          time_by_project(Harvest::Project.new, DateTime.parse("1 Nov 2010"), DateTime.parse("10 Nov 2010"), nil, :updated_since => "1 Dec 2010")
+    end
+  end
+end
